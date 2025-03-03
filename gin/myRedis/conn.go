826ea7aece2b7effd5a7ade5b807a4fa93acc2fd
胡荣潇 <14401074+Hu_BanXian@user.noreply.gitee.com>
2025-03-03 21:34:33 +0800

    修改redis重连逻辑

--- gin/myRedis/conn.go
+++ gin/myRedis/conn.go
@@ -3,19 +3,24 @@ package myRedis
 import (
 	"fmt"
 	"strconv"
+	"time"
 
 	"github.com/gomodule/redigo/redis"
 )
 
 func Connect(addr string, password string, db string, dbIndex int) redis.Conn {
 	database, _ := strconv.Atoi(db)
-	conn, err := redis.Dial("tcp", addr, redis.DialPassword(password), redis.DialDatabase(database+dbIndex))
-	if err != nil {
-		fmt.Println(err)
-		return nil
+	for range 3 {
+		conn, err := redis.Dial("tcp", addr, redis.DialPassword(password), redis.DialDatabase(database+dbIndex))
+		if err != nil {
+			fmt.Println("连接失败，正在重试...", err)
+			time.Sleep(3 * time.Second)
+			continue
+		}
+		fmt.Println("Redis连接成功")
+		return conn
 	}
-	fmt.Println("Redis连接成功")
-	return conn
+	return nil
 }
 
 func Close(conn redis.Conn) {
